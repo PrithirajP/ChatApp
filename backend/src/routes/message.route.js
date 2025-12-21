500ea072b3095de1e8663533b5a76f0553359fbb
Prithiraj Paul <paulprithiraj89@gmail.com>
2025-12-22 00:02:23 +0530

    Revert "message routes implemented"

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,16 +1,9 @@
 import express from "express";
-import { getAllContacts, getMessagesByUserId, sendMessage, getChatPartners } from "../controllers/message.controller.js";
-import { protectRoute } from "../middleware/auth.middleware.js";
-import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router();
 
-router.use(arcjetProtection, protectRoute);
-
-router.get("/contacts", getAllContacts);
-router.get("/chats", getChatPartners);
-router.get("/:id", getMessagesByUserId);
-
-router.post("/send/:id", protectRoute, sendMessage);
+router.get("/send", (req, res) => {
+    res.send("Send message endpoint");
+})
 
 export default router;
