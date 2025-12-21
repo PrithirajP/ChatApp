d12db9966f7a38decca2e7ed52df99a7163fd565
Prithiraj Paul <prithiraj89@gmail.com>
2025-12-21 18:05:45 +0530

    Fix login and logout controller bugs

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -68,6 +68,11 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
+
+        if(!email || !password) {
+            return res.status(400).json({message: "All fields are required"});
+        }
+
         const user = await User.findOne({email});
         if(!user) return res.status(400).json({message: "Invalid credentials"});
         // never tell the client which one is incorrect
@@ -94,7 +99,7 @@ export const login = async (req, res) => {
 export const logout = (_, res) => {
     try {
         res.cookie("jwt", "", { maxAge: 0 });
-        res.status(200).json({ message: "Logged out successfully "});
+        res.status(200).json({ message: "Logged out successfully"});    
     } catch(error) {
         console.error("Error in logout module: ", error);
         res.status(500).json({message: "Internal server error"});
